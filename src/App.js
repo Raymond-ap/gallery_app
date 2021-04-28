d059452474d531e27eebf858aacabe7776f65d31
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-28 03:04:52 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { API_KEY } from "./request";
+import Image from "./components/Image";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoadinging, setIsLoadinging] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const fectData = async () => {
+      const request = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${term}&image_type=photo
+      `);
+      const _images = await request.json();
+      setImages(_images.hits);
+      setIsLoadinging(false);
+    };
+    fectData();
+  }, [term]);
+
+  console.log(images);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" className="w-full" alt="..."/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-          <strong>Views: </strong>
-          400
-          </li>
-          <li>
-          <strong>Downloads: </strong>
-          400
-          </li>
-          <li>
-          <strong>Likes: </strong>
-          400
-          </li>
-        </ul>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => {
+          return <Image key={image.id} image={image} />;
+        })}
       </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-font-semibold text-gray-700 mr-2">
-          #tag1
-          </span>        
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-font-semibold text-gray-700 mr-2">
-          #tag1
-          </span>        
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-font-semibold text-gray-700 mr-2">
-          #tag1
-          </span>        
-      </div>
-      </div>
+    </div>
   );
 }
 
 export default App;
- 
